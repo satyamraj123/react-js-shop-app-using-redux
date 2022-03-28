6bed1e78539a1f1d4d397d3311748ffa8d12135a
satyamraj <satyamraj-creator@users.noreply.github.com>
2022-03-28 17:19:10 +0530

    updating cart in firebase

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import Cart from './components/Cart/Cart';
-import Layout from './components/Layout/Layout';
-import Products from './components/Shop/Products';
-import { useSelector } from 'react-redux';
+import Cart from "./components/Cart/Cart";
+import Layout from "./components/Layout/Layout";
+import Products from "./components/Shop/Products";
+import { useSelector } from "react-redux";
+import { useEffect } from "react";
 function App() {
-  const cartIsVisible=useSelector((state)=>state.ui.cartIsVisible);
+  const cartIsVisible = useSelector((state) => state.ui.cartIsVisible);
+  const cart = useSelector((state) => state.cart);
+
+  //whenever cart updates this useeffect will automatically update cart in firebase
+  useEffect(() => {
+    fetch("https://matchr-8e94e-default-rtdb.firebaseio.com/cart.json", {
+      method: "PUT",
+      body: JSON.stringify(cart),
+    });
+  }, [cart]);
   return (
     <Layout>
-      {cartIsVisible&&<Cart />}
+      {cartIsVisible && <Cart />}
       <Products />
     </Layout>
   );
